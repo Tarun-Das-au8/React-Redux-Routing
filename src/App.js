4f6ccea08882b5963837de6afcce84bf9313056a
Tarun-Das-au8 <imtarun79@gmail.com>
2021-05-21 16:59:42 +0530

    app root

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import { HomePage } from "./containers/HomePage";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { UserPage } from "./containers/UserPage";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/user/:userId" component={UserPage} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
